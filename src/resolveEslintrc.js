cef3bca76725c8fab4105bfcce738f5e6c03c378
stevewang <stevewang@easyops.cn>
2020-10-17 11:34:16 +0800

    fix: fix eslintrc

--- src/resolveEslintrc.js
+++ src/resolveEslintrc.js
@@ -24,6 +24,11 @@ module.exports = function resolveEslintrc(flags) {
   if (flags.react) {
     eslintrc.extends.push("plugin:react/recommended");
     eslintrc.plugins.push("react-hooks");
+    eslintrc.settings = {
+      react: {
+        version: "detect",
+      },
+    };
   }
   if (flags.prettier) {
     eslintrc.extends.push("prettier");
@@ -34,10 +39,13 @@ module.exports = function resolveEslintrc(flags) {
   if (flags.jest) {
     eslintrc.env.jest = true;
     eslintrc.overrides.push({
-      files: ["*.config.js"],
+      files: ["*.config.js", "__jest__/**/*.js"],
       env: {
         node: true,
         browser: false,
+      },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
       },
     });
   }
